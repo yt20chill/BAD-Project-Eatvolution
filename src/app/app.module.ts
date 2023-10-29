d87661069cd8022fa8f6553a70ed9ab93696e231
yt20chill <ytelainechiu@gmail.com>
2023-10-29 13:53:26 +0800

    feat: serve public folder in app module
    need to fix the prefix. want it to be in root / rather than /public

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { Logger, Module } from "@nestjs/common";
 import { ScheduleModule } from "@nestjs/schedule";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [
+    ScheduleModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", "..", "public"),
+      serveRoot: "/public",
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, Logger],
 })
