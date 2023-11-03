a15f550cd9c4148f2acde9e3da41576434a64a86
yt20chill <ytelainechiu@gmail.com>
2023-11-03 11:40:58 +0800

    added auto-generated types for db

---
+++ node_server/models/dbModels.ts
@@ -0,0 +1,93 @@
+export type User = {
+  id?: null | number;
+  username: string;
+  hash_password: string; // char(60)
+  money: number;
+  total_money: number;
+  created_at: string;
+  updated_at: string;
+};
+
+export type Category = {
+  id?: null | number;
+  name: string;
+  created_at: string;
+  updated_at: string;
+};
+
+export type SlimeType = {
+  id?: null | string; // uuid
+  name: string;
+  category_id: number;
+  category?: Category;
+  required_count: number;
+  created_at: string;
+  updated_at: string;
+};
+
+export type Slime = {
+  id?: null | number;
+  owner_id: number;
+  owner?: User;
+  slime_type_id: string; // uuid
+  slime_type?: SlimeType;
+  calories: number;
+  extra_calories: number;
+  is_archived: boolean;
+  created_at: string;
+  updated_at: string;
+};
+
+export type Food = {
+  id?: null | number;
+  name: string;
+  cost: number;
+  calories: number;
+  protein: number;
+  fat: number;
+  cholesterol: number;
+  carb: number;
+  fibre: number;
+  sugar: number;
+  sodium: number;
+  potassium: number;
+  created_at: string;
+  updated_at: string;
+};
+
+export type FoodCategory = {
+  id?: null | number;
+  food_id: number;
+  food?: Food;
+  category_id: number;
+  category?: Category;
+  created_at: string;
+  updated_at: string;
+};
+
+export type SlimeFood = {
+  id?: null | number;
+  slime_id: number;
+  slime?: Slime;
+  food_id: number;
+  food?: Food;
+  created_at: string;
+};
+
+export type UserFoodCollection = {
+  id?: null | number;
+  user_id: number;
+  user?: User;
+  food_id: number;
+  food?: Food;
+  created_at: string;
+};
+
+export type UserSlimeTypeCollection = {
+  id?: null | number;
+  user_id: number;
+  user?: User;
+  slime_type_id: string; // uuid
+  slime_type?: SlimeType;
+  created_at: string;
+};
