8b4d3b03a7c2ae9d5c1e99553bbdf825a449d916
yt20chill <ytelainechiu@gmail.com>
2023-11-04 15:42:14 +0800

    ci: added test cases for authService and authController

---
+++ node_server/src/utils/testUtils.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Request, Response } from "express";
+import { RedisClientType } from "redis";
+import { Server } from "socket.io";
+
+export const mockSocketIO = () => {
+  const emit = jest.fn(() => null);
+  return {
+    to: jest.fn(() => ({ emit })),
+  } as unknown as Server;
+};
+
+export const mockRequest = () => {
+  return {
+    session: {},
+    body: {},
+    params: {},
+  } as unknown as Request;
+};
+
+export const mockResponse = () => {
+  const res: any = {};
+  res.status = jest.fn((_status: number) => res);
+  res.json = jest.fn(() => null);
+  return res as Response;
+};
+
+export const mockRedis = () => {
+  const get = jest.fn((_key: string) => null);
+  const setEx = jest.fn((_key: string, _expiry: number, _result: string) => null);
+  return {
+    get,
+    setEx,
+  } as unknown as RedisClientType;
+};
