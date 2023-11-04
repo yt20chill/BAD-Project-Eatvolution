a3595c018a0b9cb7442f37671b8be266cf384be6
yt20chill <ytelainechiu@gmail.com>
2023-11-04 21:34:09 +0800

    chore: added seed file for basic food seeding

---
+++ node_server/src/db/seeds/01-food.ts
@@ -0,0 +1,32 @@
+import { Knex } from "knex";
+import { Food, GeneralOmitField } from "models/dbModels";
+import path from "path";
+import DbUtils, { nutritionToNumber } from "../../utils/dbUtils";
+import { logger } from "../../utils/logger";
+
+export async function seed(knex: Knex): Promise<void> {
+  const trx = await knex.transaction();
+  try {
+    // Deletes ALL existing entries
+    await trx("food").del();
+    await trx.raw("ALTER SEQUENCE food_id_seq RESTART WITH 1");
+
+    // Inserts seed entries
+    const food = nutritionToNumber(
+      await DbUtils.csvToTable<Omit<Food, GeneralOmitField | "created_by">>(
+        path.join(__dirname, "..", "/food.csv")
+      )
+    );
+    // insert food in ascending order by cost
+    await trx.batchInsert(
+      "food",
+      food.sort((a, b) => a.cost - b.cost),
+      50
+    );
+    trx.commit();
+    logger.info("food successfully inserted");
+  } catch (error) {
+    logger.error(error.message);
+    trx.rollback();
+  }
+}
---
+++ node_server/src/utils/dbUtils.ts
@@ -0,0 +1,37 @@
+import csvParser from "csv-parser";
+import fs from "fs";
+import { Food, GeneralOmitField } from "models/dbModels";
+
+export default class DbUtils {
+  /**
+   * read csv file and convert the data into seed-able objects
+   * @param filepath
+   * @returns Promise array of objects for insertion to table
+   */
+  static csvToTable<T = unknown>(filepath: string): Promise<Array<T>> {
+    return new Promise((resolve, reject) => {
+      const result: Array<T> = [];
+      fs.createReadStream(filepath)
+        .pipe(csvParser())
+        .on("data", (data) => result.push(data))
+        .on("end", () => resolve(result))
+        .on("error", (err) => reject(err));
+    });
+  }
+}
+
+type T = Omit<Food, GeneralOmitField | "created_by">;
+export const nutritionToNumber = (food: T[]) => {
+  return food.reduce(
+    (result, f) => {
+      for (const key of Object.keys(f)) {
+        if (key === "name") continue;
+        f[key] = parseFloat(f[key]);
+        if (key === "cost") f[key] = Math.floor(f[key]);
+      }
+      result.push(f);
+      return result;
+    },
+    [] as Omit<Food, GeneralOmitField | "created_by">[]
+  );
+};
