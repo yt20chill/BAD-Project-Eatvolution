420a49ba97bb9334121f0c08329fe97128d13a9c
Jade Yik <jadeyik@Jades-MacBook-Air.local>
2023-11-07 14:48:06 +0800

    auth.service base on testcase

--- node_server/src/auth/auth.service.ts
+++ node_server/src/auth/auth.service.ts
@@ -17,54 +17,60 @@ export default class AuthService implements AuthServiceHelper {
     return await bcrypt.compare(plain, hash);
   };
 
-  // isExisting(username: string): Promise<number> {
-  //   throw new Error("Method not implemented.");
-  // }
+  isExisting = async (username: string): Promise<number> => {
+    const db_UsernameChecking = this.knex<User>("user")
+      .select("id")
+      .where("username", username)[0];//if match success =>login, false => SignUp
+    if (db_UsernameChecking.length === 0) return -1 //match false => SignUp
+    return db_UsernameChecking.id; //match success => login
+  }
 
-  signUp = async(username: string, password: string): Promise<number> => {
+  signUp = async (username: string, password: string): Promise<number> => {
+    const isUserExist = await this.isExisting(username)
+    if (isUserExist !== -1) return isUserExist
    
-    const db_user = this.knex<User>("user")
-    .select("username", "id")
-    .where("username", username)[0];
+    if (username.length || password.length === 0 || password.length > 60 || typeof username && typeof password === "undefined") throw new BadRequestError()
 
+    if (isUserExist === -1) {
+      await isUserExist
       let newUser = {
         username: username,
         password: password
-      } 
+      }
       await this.knex("user").insert({ username: username, hash_password: await this.hashPassword(newUser.password) })
-      return db_user.id
+      }
+      const db_user = this.knex<User>("user")
+      .select("id", "username", "hash_password")
+      .where("username", username)[0];
+
+    return db_user.id
   }
 
   login = async (username: string, password: string): Promise<number> => {
     const db_password = await this.knex<User>("user")
       .select("hash_password", "id")
       .where("username", username)[0];
-    if (!db_password) throw new NotFoundError();
+    if (!db_password) throw new NotFoundError();// username checking
     const isPasswordValid = await this.checkPassword(password, db_password);
-    if (!isPasswordValid) throw new BadRequestError();
+    if (!isPasswordValid) throw new BadRequestError();// password checking
+    if(!db_password || !isPasswordValid) return -1
     return db_password.id;
   };
 
 
   oauthLogin = async (email: string): Promise<number> => {
-    if(!email) throw new NotFoundError();
+    if (!email) throw new NotFoundError();
     const isUsernameValid = await this.knex<User>("user")
       .select("username", "id")
       .where("username", email)[0];
 
     if (!isUsernameValid) {
-     let newUser = {
+      let newUser = {
         username: email,
-        password: crypto.randomBytes(20).toString('hex') 
+        password: crypto.randomBytes(20).toString('hex')
       }
       await this.knex("user").insert({ username: email, hash_password: await this.hashPassword(newUser.password) })
     }
     return isUsernameValid.id;
-
-
   }
-
-
-
-
 }
