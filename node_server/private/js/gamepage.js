ad2dc5f2aed330afd99c702006fc8dd8777ab712
chonglaiki <chonglaiki@gmail.com>
2023-11-09 22:22:40 +0800

    add fetch for food card by DB

--- node_server/private/js/gamepage.js
+++ node_server/private/js/gamepage.js
@@ -70,3 +70,65 @@ function moveCharacter() {
 
 // 调用移动函数开始主角图像的移动
 moveCharacter();
+
+function showPopupMenu() {
+    var popup = document.getElementById("popupMenu");
+    popup.style.display = "block";
+}
+
+function hidePopupMenu() {
+    var popup = document.getElementById("popupMenu");
+    popup.style.display = "none";
+}
+
+var footContainer = document.getElementById('footcontainerID');
+var isFootContainerVisible = false;
+
+document.addEventListener('mousedown', function (event) {
+    var targetElement = event.target;
+    console.log(targetElement)
+    // 检查点击事件发生时的目标元素是否为footContainer或其内部元素
+    var isClickInsideFootContainer = footContainer.contains(targetElement);
+
+    if (!isClickInsideFootContainer && isFootContainerVisible) {
+        // 点击了footContainer以外的地方且footContainer可见
+        footContainer.style.display = 'none';
+        isFootContainerVisible = false;
+    }
+});
+
+// 点击food shop按钮时显示footContainer
+document.getElementById('foodShopButton').addEventListener('click', async function () {
+    footContainer.style.display = 'block';
+    isFootContainerVisible = true;
+    await getShopItems()
+});
+
+async function getShopItems() {
+    const res = await fetch("/api/shop");
+    const { success, result } = await res.json();
+    if (!success) return alert("something went wrong");
+
+    // 更新每个卡片的 HTML 内容
+    result.forEach((item, index) => {
+        const { name, calories, cost } = item;
+        const cardElement = document.getElementById(`card${index + 1}`);
+        cardElement.querySelector(".name").textContent = name;
+        cardElement.querySelector(".calories").textContent = `Calories: ${calories}`;
+        cardElement.querySelector(".cost").textContent = `Cost: ${cost}`;
+    });
+}
+
+async function login(username, password) {
+    //for fetch method other than get (e.g. post)
+    const res = await fetch("/auth/login", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        // body: JSON.stringify({username: username, password:password}) equals below
+        body: JSON.stringify({ username, password })
+
+    })
+    const { success, result } = await res.json()
+}
