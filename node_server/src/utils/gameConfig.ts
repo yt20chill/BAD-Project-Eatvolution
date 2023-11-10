a38e81bd46a085e2897bdddccef2cfd7e35ec357
yt20chill <ytelainechiu@gmail.com>
2023-11-11 00:54:24 +0800

    fix: change game config to class in order to access its own variables

--- node_server/src/utils/gameConfig.ts
+++ node_server/src/utils/gameConfig.ts
@@ -1,8 +1,8 @@
-const gameConfig = {
-  EARNING_RATE_CONSTANT: 1,
-  MONEY_UPDATE_SCHEDULE: "* /5 * * * *", // every 5 minutes
-  SHOP_REFRESH_SCHEDULE: "0 0 8,13,19 * * *", // every 8am, 1pm, 7pm
-  FOOD_NUM_ALLOWED: 12, // number of food allowed in shop
-};
-
-export default gameConfig;
+export default class GameConfig {
+  static readonly EARNING_RATE_CONSTANT = 1;
+  static readonly MONEY_UPDATE_SCHEDULE = "* /5 * * * *"; // every 5 minutes
+  static readonly SHOP_REFRESH_SCHEDULE = "0 0 8,13,19 * * *"; // every 8am, 1pm, 7pm
+  static readonly FOOD_NUM_ALLOWED = 12; // number of food allowed in shop
+  static readonly CHEAP_FOOD_NUM = 1; // min number of cheap food in shop
+  static readonly CHEAP_FOOD_MAX_INDEX = Math.min(this.CHEAP_FOOD_NUM + 5, this.FOOD_NUM_ALLOWED);
+}
