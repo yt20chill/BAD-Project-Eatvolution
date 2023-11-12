ee87a9c4d06c470c14e9c00e86db74b779328541
yt20chill <ytelainechiu@gmail.com>
2023-11-13 00:16:09 +0800

    fix: correctly calculate user earn rate

--- node_server/src/user/user.service.ts
+++ node_server/src/user/user.service.ts
@@ -5,7 +5,7 @@ import { RedisUser, UserFinancialStatus } from "../../models/models";
 import { UserServiceHelper } from "../../models/serviceModels";
 import DbUtils from "../utils/dbUtils";
 import { InternalServerError, UnauthorizedError } from "../utils/error";
-import gameConfig from "../utils/gameConfig";
+import GameConfig from "../utils/gameConfig";
 
 export default class UserService implements UserServiceHelper {
   constructor(
@@ -13,43 +13,37 @@ export default class UserService implements UserServiceHelper {
     private readonly redis: RedisClientType
   ) {}
   private getRedisUser = async (userId: number): Promise<RedisUser> => {
+    // return {} as RedisUser;
     return JSON.parse(await this.redis.get(`${userId}`)) ?? ({} as RedisUser);
   };
-  private validateRedisUser = async (user: RedisUser): Promise<boolean> => {
-    const { money, totalMoney, earningRate, lastUpdated } = user;
+  private validateRedisUser = (user: RedisUser): boolean => {
+    if (Object.keys(user).length < 4) return false;
+    const { money, total_money, earning_rate, updated_at } = user;
     // simpler way to except the falsy expression when values === 0
-    return !(money < 0 || totalMoney < 0 || earningRate < 0 || !lastUpdated);
+    return !(money < 0 || total_money < 0 || earning_rate < 0 || !updated_at);
   };
   private getEarningRate = async (userId: number): Promise<number> => {
     const user = await this.getRedisUser(userId);
-    if (user.earningRate) return user.earningRate;
+    if (user.earning_rate) return user.earning_rate;
     // select slime id and earn rate multiplier of it's type
-    const subquery = this.knex("slime")
-      .select("slime.id as slime_id", "slime_type.earn_rate_multiplier")
-      .join("user", "slime.owner_id", "user.id")
-      .join("slime_type", "slime.slime_type_id", "slime_type.id")
-      .where("user.id", userId)
-      .andWhere("slime.calories", ">", 0);
-    // apply formula: userEarnRate = (protein * earnRateMultiplier * EARNING_RATE_CONSTANT), sum all for all food.protein
-    // knex.sum() only supports summing up a single column
     const { userEarnRate } = await this.knex("slime_food")
-      .with("user_slime", subquery)
       .select<{ userEarnRate: string }>(
-        this.knex.raw(`SUM(food.protein * user_slime.earn_rate_multiplier * ?) as "userEarnRate"`, [
-          gameConfig.EARNING_RATE_CONSTANT,
+        this.knex.raw(`SUM(food.protein * slime_type.earn_rate_multiplier * ?) as "userEarnRate"`, [
+          GameConfig.EARNING_RATE_CONSTANT,
         ])
       )
-      // inner join: slime.id must be in user_slime
-      .join("user_slime", "user_slime.slime_id", "slime_food.slime_id")
-      .join("food", "food.id", "slime_food.food_id")
       .join("slime", "slime.id", "slime_food.slime_id")
+      .join("food", "slime_food.food_id", "food.id")
+      .join("slime_type", "slime_type.id", "slime.slime_type_id")
+      .where("slime.calories", ">", 0)
+      .groupBy("slime.owner_id")
+      .having("slime.owner_id", "=", userId)
       .first();
     // if all slimes calories = 0, userEarnRate = 0
     if (userEarnRate === undefined) return 0;
-    // if userEarnRate = 0 (i.e. total_protein = 0), userEarnRate = 1
-    user.earningRate = +userEarnRate || 1;
+    user.earning_rate = Math.max(1, +userEarnRate);
     await this.redis.setEx(`${userId}`, 60, JSON.stringify(user));
-    return user.earningRate;
+    return user.earning_rate;
   };
   getUserLatestFinancialStatus = async (userId: number): Promise<UserFinancialStatus> => {
     await this.updateUserFinancialStatus(userId);
@@ -62,8 +56,8 @@ export default class UserService implements UserServiceHelper {
       .where("id", userId)
       .first();
     if (money < 0 || total_money < 0 || !updated_at) throw new UnauthorizedError();
-    const earningRate = await this.getEarningRate(userId);
-    user = { money, totalMoney: total_money, earningRate, lastUpdated: new Date(updated_at) };
+    const earning_rate = await this.getEarningRate(userId);
+    user = { money, total_money: total_money, earning_rate, updated_at: new Date(updated_at) };
     this.redis.setEx(`${userId}`, 60, JSON.stringify(user));
     return { id: userId, ...user };
   };
@@ -74,34 +68,34 @@ export default class UserService implements UserServiceHelper {
    */
   updateUserFinancialStatus = async (userId: number): Promise<boolean> => {
     const user = await this.getRedisUser(userId);
-    let totalMoney: number,
+    let total_money: number,
       money: number,
-      earningRate: number,
-      lastUpdated: Date,
+      earning_rate: number,
+      updated_at: Date,
       elapsedSeconds: number;
     const now = new Date();
-    if (!(await this.validateRedisUser(user))) {
+    if (!this.validateRedisUser(user)) {
       // get elapsed seconds, money, and total money from db
       const query = await this.knex("user")
-        .select("updated_at", "money", "total_money as totalMoney")
+        .select("updated_at", "money", "total_money")
         .where("id", userId)
         .first();
-      ({ totalMoney, money } = query);
-      if (money < 0 || totalMoney < 0) throw new UnauthorizedError();
-      earningRate = await this.getEarningRate(userId);
+      ({ total_money, money } = query);
+      if (money < 0 || total_money < 0) throw new UnauthorizedError();
+      earning_rate = await this.getEarningRate(userId);
       elapsedSeconds = DbUtils.calculateElapsedTimeInSeconds(query, now);
     } else {
-      ({ totalMoney, money, lastUpdated, earningRate } = user);
-      elapsedSeconds = DbUtils.calculateElapsedTimeInSeconds({ updated_at: lastUpdated }, now);
+      ({ total_money, money, updated_at, earning_rate } = user);
+      elapsedSeconds = DbUtils.calculateElapsedTimeInSeconds({ updated_at }, now);
     }
     // update money
-    if (!(elapsedSeconds >= 0 || money >= 0 || totalMoney >= 0 || earningRate >= 0))
+    if (!(elapsedSeconds >= 0 || money >= 0 || total_money >= 0 || earning_rate >= 0))
       throw new InternalServerError();
 
-    const earnedMoney = Math.floor(elapsedSeconds * earningRate);
+    const earnedMoney = Math.floor(elapsedSeconds * earning_rate);
     // update db
     const updateMoney = +money + earnedMoney;
-    const updateTotalMoney = +totalMoney + earnedMoney;
+    const updateTotalMoney = +total_money + earnedMoney;
     await this.knex("user")
       .update({
         money: updateMoney,
@@ -111,9 +105,9 @@ export default class UserService implements UserServiceHelper {
       .where("id", userId);
     // update redis
     user.money = updateMoney;
-    user.totalMoney = updateTotalMoney;
-    user.earningRate = earningRate;
-    user.lastUpdated = now;
+    user.total_money = updateTotalMoney;
+    user.earning_rate = earning_rate;
+    user.updated_at = now;
     await this.redis.setEx(`${userId}`, 60, JSON.stringify(user));
     return true;
   };
