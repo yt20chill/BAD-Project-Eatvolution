972331d456779c8b1fc962ae52dccb37a03ecee0
Jade Yik <jadeyik@Jades-MacBook-Air.local>
2023-11-13 14:45:24 +0800

    up

--- node_server/private/collection.js
+++ node_server/private/collection.js
@@ -1,23 +1,24 @@
 // 假设您有一个包含卡片数据的数组
 const cardData = [
-    { id: 1, title: "Card 1", content: "Content 1" },
-    { id: 2, title: "Card 2", content: "Content 2" },
-    // ... 其他卡片数据
+  { id: 1, title: "Card 1", content: "Content 1" },
+  { id: 2, title: "Card 2", content: "Content 2" },
+  // ... 其他卡片数据
 ];
 
 // 获取放置卡片的容器元素
-const cardsContainer = document.getElementById("cards-container");
+// Lai ka
+// const cardsContainer = document.getElementById("cards-container");
 
-// 使用 for 循环生成卡片并添加到容器中
-for (let i = 0; i < 150; i++) {
-    const card = cardData[i];
-    const cardElement = `
-      <div class="card">
-        <h2>${card.title}</h2>
-        <p>${card.content}</p>
-      </div>
-    `;
-    cardsContainer.innerHTML += cardElement;
-}
+// // 使用 for 循环生成卡片并添加到容器中
+// for (let i = 0; i < 150; i++) {
+//   const card = cardData[i];
+//   const cardElement = `
+//       <div class="card">
+//         <h2>${card.title}</h2>
+//         <p>${card.content}</p>
+//       </div>
+//     `;
+//   cardsContainer.innerHTML += cardElement;
+// }
 
 // collection page js
---
+++ node_server/private/js/auth.js
@@ -0,0 +1,11 @@
+document.getElementById("logout").addEventListener("click", async function (e) {
+    e.preventDefault();
+
+    const res = await fetch('/auth/logout');
+
+    if (res.ok) {
+        window.location.href = '/index.html';
+    } else {
+        console.error('Logout failed.');
+    }
+});
