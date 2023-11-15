01e58a8bbc3a486c427fe2983ad66c09908e3470
yt20chill <ytelainechiu@gmail.com>
2023-11-15 11:08:16 +0800

    feat: show earn rate on screen

--- node_server/private/js/gamepage.js
+++ node_server/private/js/gamepage.js
@@ -2,23 +2,23 @@ var closeBtn = document.getElementById("closeBtn");
 var popup = document.getElementById("popup");
 
 closeBtn.addEventListener("click", function () {
-    popup.classList.remove("show"); // 移除 show 类，使其回到初始位置
+  popup.classList.remove("show"); // 移除 show 类，使其回到初始位置
 });
 
 // 显示 popup 元素
-async function showPopup() {
-    popup.classList.add("show"); // 添加 show 类，使其从左侧弹出
-    document.querySelector("#gamecontainer").classList.add("blur");
+function showPopup() {
+  popup.classList.add("show"); // 添加 show 类，使其从左侧弹出
+  document.querySelector("main").classList.add("blur");
 }
 
 function hidePopup() {
-    popup.classList.remove("show"); // 移除 show 类，使其回到初始位置
-    document.querySelector("#gamecontainer").classList.remove("blur");
+  popup.classList.remove("show"); // 移除 show 类，使其回到初始位置
+  document.querySelector("main").classList.remove("blur");
 }
 
 // 移除初始的 hidden 类
 window.addEventListener("load", function () {
-    popup.classList.remove("hidden");
+  popup.classList.remove("hidden");
 });
 
 // 史來姆我行為 //
@@ -49,104 +49,111 @@ var moveDirection = 1; // 1 表示向右移动，-1 表示向左移动
 
 // 定义主角图像的移动函数
 function moveCharacter() {
-    if (moveDirection == 1) {
-        character.classList.remove("face_left");
-    } else {
-        character.classList.add("face_left");
-    }
-    // 获取主角图像的当前左边距
-    var currentLeft = parseInt(character.style.left);
+  if (moveDirection == 1) {
+    character.classList.remove("face_left");
+  } else {
+    character.classList.add("face_left");
+  }
+  // 获取主角图像的当前左边距
+  var currentLeft = parseInt(character.style.left);
 
-    // 判断是否到达移动范围的边界，若是则改变移动方向
-    if (currentLeft <= minLeft || currentLeft >= maxLeft) {
-        moveDirection *= -1;
-    }
+  // 判断是否到达移动范围的边界，若是则改变移动方向
+  if (currentLeft <= minLeft || currentLeft >= maxLeft) {
+    moveDirection *= -1;
+  }
 
-    // 根据移动方向更新主角图像的左边距
-    var newLeft = currentLeft + moveSpeed * moveDirection;
-    character.style.left = newLeft + "px";
+  // 根据移动方向更新主角图像的左边距
+  var newLeft = currentLeft + moveSpeed * moveDirection;
+  character.style.left = newLeft + "px";
 
-    // 每隔一段时间执行一次移动函数，实现连续移动效果
-    setTimeout(moveCharacter, 50); // 可根据需要进行调整
+  // 每隔一段时间执行一次移动函数，实现连续移动效果
+  setTimeout(moveCharacter, 50); // 可根据需要进行调整
 }
 
 function showPopupMenu() {
-    var popup = document.getElementById("popupMenu");
-    popup.style.display = "block";
+  var popup = document.getElementById("popupMenu");
+  document.querySelector(":root").setAttribute("SHOP", "true");
+  document.querySelector("main").classList.add("blur");
+  //popup.style.display = "block";
 }
 
-function hidePopupMenu() {
-    var popup = document.getElementById("popupMenu");
-    popup.style.display = "none";
-}
+// function hidePopupMenu() {
+//     var popup = document.getElementById("popupMenu");
+//     document.querySelector(":root").removeAttribute("SHOP")
+//     document.querySelector("main").classList.remove("blur");
+//     //popup.style.display = "none";
+// }
 
 // log out api
 
 async function getUserFinance() {
-    try {
-        const res = await fetch("/api/user/finance");
-        if (!res.ok) {
-            throw new Error("Failed to get user data");
-        }
-        const { success, result } = await res.json();
-        if (!success) {
-            throw new Error("Failed to get user data");
-        }
-        if (result.money < 0 || result.earning_rate < 0) return;
-        user.money = result.money;
-        user.earningRate = result.earning_rate;
-    } catch (error) {
-        console.error(error);
-        // alert("Failed to get user data");
+  try {
+    const res = await fetch("/api/user/finance");
+    if (!res.ok) {
+      throw new Error("Failed to get user data");
     }
+    const { success, result } = await res.json();
+    if (!success) {
+      throw new Error("Failed to get user data");
+    }
+    if (result.money < 0 || result.earning_rate < 0) return;
+    user.money = result.money;
+    user.earningRate = result.earning_rate;
+    document.querySelector("#earn_rate").textContent = `${user.earningRate}`;
+  } catch (error) {
+    console.error(error);
+    // alert("Failed to get user data");
+  }
 }
 
 async function getSlimeData() {
-    try {
-        const res = await fetch("/api/slime");
-        if (!res.ok) {
-            throw new Error("Failed to get slime data");
-        }
-        const { result } = await res.json();
-        slime.bmr = result.bmr_rate;
-        slime.cal = result.current_calories;
-        slime.maxCal = result.max_calories;
-        slime.extraCal = result.extra_calories;
-        const newType = result.slime_type.split(" ")[0]
-        if (!slime.type) slime.type = newType;
-        if (slime.type && slime.type !== newType) slime.isEvolving = true;
-        if (slime.isEvolving) evolveAnimation(newType);
-        displaySlimeData();
-    } catch (error) {
-        console.error(error);
-        // alert("Failed to get slime data");
+  try {
+    const res = await fetch("/api/slime");
+    if (!res.ok) {
+      throw new Error("Failed to get slime data");
     }
+    const { result } = await res.json();
+    slime.bmr = result.bmr_rate;
+    slime.cal = result.current_calories;
+    slime.maxCal = result.max_calories;
+    slime.extraCal = result.extra_calories;
+    const newType = result.slime_type.split(" ")[0];
+    if (!slime.type) slime.type = newType;
+    if (slime.type && slime.type !== newType) slime.isEvolving = true;
+    if (slime.isEvolving) evolveAnimation(newType);
+    displaySlimeData();
+  } catch (error) {
+    console.error(error);
+    // alert("Failed to get slime data");
+  }
 }
 function displaySlimeData() {
-    if (slime.calories < 0 || slime.bmr < 0) return;
-    document.getElementById("slime_character").src = `./img/${slime.type}/move.gif`;
-    document.getElementById("slimeStableIcon").src = `./img/${slime.type}/die.gif`;
-    const displayType = slime.type === "Skinny" ? "Skinny Fat" : slime.type;
-    document.querySelector(".slime_type").innerHTML = `<span>Type</span> <b>${displayType}</b>`;
-    document.querySelector(
-        ".current_calories"
-    ).innerHTML = `<span> Calories </span> <b>${slime.cal}/${slime.maxCal} </b>`;
-    document.querySelector(".max_calories").innerHTML = `<span> Extra Calories</span> <b>${slime.extraCal ?? 0
-        }</b>`;
+  if (slime.calories < 0 || slime.bmr < 0) return;
+  document.getElementById("slime_character").src = `./img/${slime.type}/move.gif`;
+  document.getElementById("slimeStableIcon").src = `./img/${slime.type}/die.gif`;
+  const displayType = slime.type === "Skinny" ? "Skinny Fat" : slime.type;
+  document.querySelector(".slime_type").innerHTML = `<span>Type</span> <b>${displayType}</b>`;
+  document.querySelector(
+    ".current_calories"
+  ).innerHTML = `<span> Calories </span> <b>${slime.cal}/${slime.maxCal} </b>`;
+  document.querySelector(".max_calories").innerHTML = `<span> Extra Calories</span> <b>${slime.extraCal ?? 0
+    }</b>`;
 }
 function updateSlimeCal() {
-    if (slime.cal < 0 || slime.bmr < 0) return;
-    if (slime.cal === 0 && slime.extraCal > 0) slime.extraCal -= slime.bmr;
-    slime.cal = Math.max(0, slime.cal - slime.bmr);
-    if (slime.cal === 0) user.earningRate = 0;
-    document.querySelector(".current_calories b").innerText = `${Math.round(slime.cal)}/${slime.maxCal}`;
-    document.querySelector(".max_calories b").innerText = `${Math.round(slime.extraCal) ?? 0}`;
+  if (slime.cal < 0 || slime.bmr < 0) return;
+  if (slime.cal === 0 && slime.extraCal > 0) slime.extraCal -= slime.bmr;
+  slime.cal = Math.max(0, slime.cal - slime.bmr);
+  if (slime.cal === 0) user.earningRate = 0;
+  document.querySelector("#earn_rate").textContent = 0;
+  document.querySelector(".current_calories b").innerText = `${Math.round(slime.cal)}/${slime.maxCal
+    }`;
+  document.querySelector(".max_calories b").innerText = `${Math.round(slime.extraCal) ?? 0}`;
 }
 
 function updateCoins() {
-    if (user.money < 0 || user.earningRate < 0) return;
-    user.money = Math.floor(user.money + user.earningRate);
-    document.querySelector(".card-text").textContent = `${user.money} `; // Update the coin balance
+  if (user.money < 0 || user.earningRate < 0) return;
+  user.money = Math.floor(user.money + user.earningRate);
+  document.querySelector("#coin_balance p").textContent = `${user.money} `; // Update the coin balance
 }
 // refresh shop bottom function
 
@@ -241,11 +248,11 @@ function updateCoins() {
 // const socket = io.connect();
 
 document.addEventListener("DOMContentLoaded", async () => {
-    // get coins
-    await getUserFinance();
-    setInterval(updateCoins, 1000);
-    // socket.on("evolving", evolveAnimation);
-    await getSlimeData();
-    slime.updateIntervalId = setInterval(updateSlimeCal, 1000);
-    moveCharacter();
+  // get coins
+  await getUserFinance();
+  setInterval(updateCoins, 1000);
+  // socket.on("evolving", evolveAnimation);
+  await getSlimeData();
+  slime.updateIntervalId = setInterval(updateSlimeCal, 1000);
+  moveCharacter();
 });
