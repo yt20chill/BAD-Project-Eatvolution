f4d314e9cd9e755e87051f22d9e731b7411541ec
yt20chill <ytelainechiu@gmail.com>
2023-11-15 21:59:32 +0800

    fix: removed unused async

--- node_server/src/utils/gameConfig.ts
+++ node_server/src/utils/gameConfig.ts
@@ -2,7 +2,7 @@ export default class GameConfig {
   static readonly USER_UPDATE_SCHEDULE = "* /5 * * * *"; // every 5 minutes
   static readonly SHOP_REFRESH_SCHEDULE = "0 0 8,13,19 * * *"; // every 8am, 1pm, 7pm
   static readonly FOOD_CATEGORY = ["Healthy", "Processed", "Empty", "Dessert"];
-  static readonly EARNING_RATE_CONSTANT = 10; // rate of earning rate
+  static readonly EARNING_RATE_CONSTANT = 50; // rate of earning rate
   static readonly BMR_CONSTANT = 50; // rate of calories reduction per second
   static readonly FOOD_NUM_ALLOWED = 11; // number of food allowed in shop
   static readonly CHEAP_FOOD_NUM = 1; // min number of cheap food in shop
